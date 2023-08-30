6a4c3a2070063183647c132187bbee38e32632b1
Vyacheslav Goldyuk <kuzzyatina@gmail.com>
2023-08-30 17:29:01 +0300

    End of section 10

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -7,29 +7,41 @@ import React from 'react'
 import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai';
 import { HiCog, HiUser } from 'react-icons/hi2'
 import { signOut } from 'next-auth/react'
+import { useRouter, usePathname } from 'next/navigation';
+import { useParamsStore } from '../hooks/useParamsStore';
 
 type Props = {
     user: Partial<User>
 }
 
 export default function UserActions({ user }: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const setParams = useParamsStore(state => state.setParams);
+
+    function setWinner() {
+        setParams({ winner: user.username, seller: undefined })
+        if (pathname !== '/') router.push('/');
+    }
+
+    function setSeller() {
+        setParams({ seller: user.username, winner: undefined })
+        if (pathname !== '/') router.push('/');
+    }
+
     return (
         <Dropdown
             inline
             label={`Welcome ${user.name}`}
         >
-            <Dropdown.Item icon={HiUser}>
-                <Link href='/'>
-                    My Auctions
-                </Link>
+            <Dropdown.Item icon={HiUser} onClick={setSeller}>
+                My Auctions
             </Dropdown.Item>
-            <Dropdown.Item icon={AiFillTrophy}>
-                <Link href='/'>
-                    Auctions won
-                </Link>
+            <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+                Auctions won
             </Dropdown.Item>
             <Dropdown.Item icon={AiFillCar}>
-                <Link href='/'>
+                <Link href='/auctions/create'>
                     Sell my car
                 </Link>
             </Dropdown.Item>
@@ -45,3 +57,6 @@ export default function UserActions({ user }: Props) {
         </Dropdown>
     )
 }
+
+
+
